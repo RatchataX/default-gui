7ec5ee1a269ab694c85012f96fa819204f8b1524
RatchataX <113961073+RatchataX@users.noreply.github.com>
2022-09-20 11:21:11 +0700

    Update default-Gui.lua

--- default-Gui.lua
+++ default-Gui.lua
@@ -121,10 +121,10 @@ local themeStyles = {
     },
     RatchataX = {
         SchemeColor = Color3.fromRGB(0, 0, 0),
-        Background = Color3.fromRGB(255, 0, 0),
+        Background = Color3.fromRGB(0, 0, 0),
         Header = Color3.fromRGB(0, 0, 0),
         TextColor = Color3.fromRGB(255,255,255),
-        ElementColor = Color3.fromRGB(0, 255, 0)
+        ElementColor = Color3.fromRGB(0, 0, 0)
     }
 }
 local oldTheme = ""
